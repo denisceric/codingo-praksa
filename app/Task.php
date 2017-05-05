9351332f060bef748ef307a1a963d59897ba3558
denisceric <denisceric@icloud.com>
2017-05-05 18:39:40 +0200

    Ok, now tasks are associated wiht users, I did it my way and I don't know yet if that is the best  solution, I will find out.

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    public function tasks()
+    {
+        return $this->belongsTo(Auth::class);
+    }
 }
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -5,10 +5,11 @@
 Događaji:
 <hr>
 @foreach($tasks as $task)
+@if(Auth::id() == $task->user_id)
     <div class="row">
         <div class="col-md-10 col-md-offset-0">
             <div class="panel panel-default">
-                <div class="panel-heading"><a href="{{ $task->id }}">{{ $task->title }} </a><div style="float: right;text-align: right;color: gray;">{{ $task->created_at->toDayDateTimeString() }}</div></div>
+                <div class="panel-heading"><a href="{{ $task->id }}">{{ $task->title }}</a><div style="float: right;text-align: right;color: gray;">{{ $task->created_at->toDayDateTimeString() }}</div></div>
 
                 <div class="panel-body">
                     {{ $task->description }}
@@ -19,6 +20,7 @@ Događaji:
             </div>
         </div>
     </div>
+@endif
 @endforeach
 
 </div>
