fd8531703a3342dd05c2aab00a9b1643b9c8ad67
denisceric <denisceric@icloud.com>
2017-05-08 21:24:46 +0200

    Added custom 404 page. By the way I realize now it would be better if I created roles table to determine who is regular user and who is administrator..

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>STRANICA NIJE PRONAĐENA</title>
+	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="/css/sidebar.css" rel="stylesheet">
+</head>
+<body>
+<div class="container text-center">
+<h1>GREŠKA:</h1>
+<h3>Stranica nije pronađena.</h3>
+<a href="{!! url('/') !!}">NAZAD.</a>
+</div>
+</body>
+</html>
